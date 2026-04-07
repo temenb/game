8c87dbbd2f9a1f1350d129fb289ffc51ebf3feaf
Oleksii Temchenko <OTemchenko@fxcess.com>
2026-04-07 16:31:50 +0300

    pg-boss is in progress

--- shared/kafka/src/lib/eventWorker.ts
+++ shared/kafka/src/lib/eventWorker.ts
@@ -1,18 +1,18 @@
-import { boss } from './pgBoss';
-import { createProducer } from '@shared/kafka';
-
-export async function startEventWorker(kafkaConfig) {
-  const producer = await createProducer(kafkaConfig);
-
-  await boss.work('event.*', async job => {
-    const { name, data } = job;
-    const topic = name.replace('event.', '');
-
-    await producer.send({
-      topic,
-      messages: [{ value: JSON.stringify(data) }],
-    });
-
-    return true;
-  });
-}
+// import { boss } from './pgBoss';
+// import { createProducer } from '@shared/kafka';
+//
+// export async function startEventWorker(kafkaConfig) {
+//   const producer = await createProducer(kafkaConfig);
+//
+//   await boss.work('event.*', async job => {
+//     const { name, data } = job;
+//     const topic = name.replace('event.', '');
+//
+//     await producer.send({
+//       topic,
+//       messages: [{ value: JSON.stringify(data) }],
+//     });
+//
+//     return true;
+//   });
+// }
