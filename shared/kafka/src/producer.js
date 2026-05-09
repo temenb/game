4107d9fe9790c14d992f258ed488d15c8c751a2d
Oleksii Temchenko <temenb@gmail.com>
2026-05-09 15:04:15 +0300

    kafka works fine

--- shared/kafka/src/producer.js
+++ shared/kafka/src/producer.js
@@ -2,9 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createProducer = createProducer;
 const register_1 = require("./register");
+const kafkajs_1 = require("kafkajs");
 async function createProducer(config) {
     const kafka = (0, register_1.getKafkaInstance)(config);
-    const producer = kafka.producer();
+    const producer = kafka.producer({
+        createPartitioner: kafkajs_1.Partitioners.DefaultPartitioner,
+    });
     await producer.connect();
     return {
         send: async (topic, message) => {
--- shared/kafka/src/producer.ts
+++ shared/kafka/src/producer.ts
@@ -1,9 +1,12 @@
 import { getKafkaInstance } from './register';
 import { KafkaConfig } from './types';
+import {Partitioners} from "kafkajs";
 
 export async function createProducer(config: KafkaConfig) {
   const kafka = getKafkaInstance(config);
-  const producer = kafka.producer();
+  const producer = kafka.producer({
+    createPartitioner: Partitioners.DefaultPartitioner,
+  });
   await producer.connect();
 
   return {
