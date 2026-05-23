d6d5b18a4c4841338b2495d45e4ca208e8689170
Oleksii Temchenko <temenb@gmail.com>
2026-05-23 15:37:44 +0300

    fixing grpc in progress

--- services/battle/src/lib/consumers.ts
+++ services/battle/src/lib/consumers.ts
@@ -3,11 +3,11 @@ import {updateBattle} from "../services/battle.service";
 import {BattleObject} from "../grpc/generated/battle";
 
 
-export async function battleUpdated(message: BattleObject): Promise<void> {
+export async function battleUpdated(message: any): Promise<void> {
   try {
     logger.log('battleUpdated');
     logger.log(message);
-    await updateBattle(message);
+    await updateBattle(message as BattleObject);
   } catch (error) {
     logger.error(`[Kafka] Failed to process message`, {
       rawValue: message.userId,
